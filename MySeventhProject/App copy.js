3775d0b6ea082c28bb5b92113f9a770f958a5e17
abbygerber <130310396+abbygerber@users.noreply.github.com>
2023-04-30 22:15:00 -0600

    MySeventhProject

---
+++ MySeventhProject/App copy.js
@@ -0,0 +1,68 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+    <View style = {styles.containerButton}>
+      <Button 
+          onPress = {() => {
+            alert('You tapped button 1'); 
+          }}
+          title = "press button 1"
+    />
+    </View>
+
+    <View style = {styles.containerLayoutButton}> 
+    
+    <Button 
+          onPress = {() => {
+            alert('You tapped button 2'); 
+          }}
+          title = "press button 2"
+    />
+  
+
+    <Button 
+          onPress = {() => {
+            alert('You tapped button Great!'); 
+          }}
+          title = "Great!" 
+          color = 'green'
+    />
+
+    </View>
+
+      <View style = {styles.containerButton}>
+
+    <Button 
+          onPress = {() => {
+            alert('You tapped button Tap Me!'); 
+          }}
+          title = "Tap Me!" 
+    />
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  containerButton: {
+    margin: 20,  
+  },
+
+  containerLayoutButton: {
+    margin: 20, 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+  }
+
+});
